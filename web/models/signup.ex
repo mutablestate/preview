3f48444f0aaa88c321dcd0863db1577d467aac3e
mutablestate <benwebdev@gmail.com>
2014-12-09 04:31:51 +0700

    add timestamp fields to users and signups fixes #4

--- web/models/signup.ex
+++ web/models/signup.ex
@@ -3,5 +3,7 @@ defmodule Preview.Signup do
 
   schema "signups" do
     field :email, :string
+    field :created_at, :datetime, default: Ecto.DateTime.local
+    field :updated_at, :datetime, default: Ecto.DateTime.local
   end
 end
