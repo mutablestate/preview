055fae9e25a867f7451b3d15633e1b9eb6c8f62f
mutablestate <benwebdev@gmail.com>
2015-01-02 15:18:12 +0700

    add doc strings and find_by_username query

--- web/models/queries.ex
+++ web/models/queries.ex
@@ -2,6 +2,11 @@ defmodule Preview.Queries do
   import Ecto.Query
   alias Preview.Authenticate
 
+  @doc """
+  Query for all users
+
+  Returns list of structs
+  """
   def all_users do
     query = from user in Preview.User,
             select: user
@@ -9,6 +14,24 @@ defmodule Preview.Queries do
     Preview.Repo.all(query)
   end
 
+  @doc """
+  Query finds user by username
+
+  Returns user struct or nil
+  """
+  def find_by_username(username) do
+    query = from user in Preview.User,
+            where: user.username == ^username,
+            select: user
+
+    Preview.Repo.one(query)
+  end
+
+  @doc """
+  Query for all signups
+
+  Returns list of structs
+  """
   def all_signups do
     query = from signup in Preview.Signup,
             select: signup
@@ -16,6 +39,11 @@ defmodule Preview.Queries do
     Preview.Repo.all(query)
   end
 
+  @doc """
+  Query authenticates user
+
+  Returns user struct or nil
+  """
   def login(username, password) do
     query = from user in Preview.User,
             where: user.username == ^username,
