6361c3ce304a77177a685b812e40bc2a8dca001f
mutablestate <benwebdev@gmail.com>
2015-01-31 09:01:43 +0700

    add signups action to send the csv

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -60,4 +60,42 @@ defmodule Preview.UserController do
     |> put_flash(:notice, "You're logged out.")
     |> redirect to: root_path(conn, :index)
   end
+
+  def signups(conn, _params) do
+    signups = Preview.Queries.all_signups
+
+    csv_filename = signups
+      |> to_csv(["id", "email"])
+
+    conn
+    |> put_resp_content_type("text/csv")
+    |> put_resp_header("content-disposition", "attachment; filename=#{csv_filename}")
+    |> send_file(200, csv_filename)
+  end
+
+  defp to_csv(signups, headers) do
+    header_line = CSVLixir.write(headers)
+
+    csv_lines = signups
+      |> Enum.map(fn s -> ["#{s.id}", "#{s.email}"] end)
+      |> CSVLixir.write
+
+    csv_data = header_line <> "\n" <> csv_lines
+    csv_filename = "signups#{timestamp}.csv"
+
+    csv_filename
+    |> File.write(csv_data)
+
+    csv_filename
+  end
+
+  defp timestamp do
+    :erlang.now
+    |> :calendar.now_to_universal_time
+    |> :calendar.datetime_to_gregorian_seconds
+    |> to_epoch_time
+  end
+  defp to_epoch_time(gregorian_seconds) do
+    (gregorian_seconds) -719528*24*3600
+  end
 end
