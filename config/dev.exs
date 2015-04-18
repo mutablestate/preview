beed15957b00489512d9558b82365a555a0c56cb
mutablestate <benwebdev@gmail.com>
2015-04-18 19:30:34 +0700

    use phoenix 0.11.0 config style

--- config/dev.exs
+++ config/dev.exs
@@ -1,24 +1,39 @@
 use Mix.Config
 
+# For development, we disable any cache and enable
+# debugging and code reloading.
+#
+# The watchers configuration can be used to run external
+# watchers to your application. For example, we use it
+# with brunch.io to recompile .js and .css sources.
 config :preview, Preview.Endpoint,
-  http: [port: System.get_env("PORT") || 4000],
+  http: [port: 4000],
   debug_errors: true,
-  cache_static_lookup: false
+  code_reloader: true,
+  cache_static_lookup: false,
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch"]]
 
-# Enables code reloading for development
-config :phoenix, :code_reloader, true
+# Watch static and templates for browser reloading.
+config :preview, Preview.Endpoint,
+  live_reload: [
+    patterns: [
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif)$},
+      ~r{web/views/.*(ex)$},
+      ~r{web/templates/.*(eex)$}
+    ]
+  ]
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+# Configure your database
+config :preview, Preview.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "dev",
+  password: System.get_env("POSTGRES_PASS"),
+  database: "preview_dev"
+
+# Configure mailgun
 config :preview,
   mailgun_domain: System.get_env("MAILGUN_DOMAIN"),
   mailgun_key:    System.get_env("MAILGUN_KEY")
-
-# Config ecto repo
-config :preview, Preview.Repo,
-  database: "preview_dev",
-  username: "deploy",
-  password: "#{System.get_env("POSTGRES_PASS")}",
-  hostname: "localhost"
-
--- config/test.exs
+++ config/test.exs
@@ -1,23 +1,24 @@
 use Mix.Config
 
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
 config :preview, Preview.Endpoint,
-  http: [port: System.get_env("PORT") || 4001]
+  http: [port: 4001],
+  server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
 
+# Configure your database
+config :preview, Preview.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "dev",
+  password: System.get_env("POSTGRES_PASS"),
+  database: "preview_test",
+  size: 1,
+  max_overflow: false
+
+# Configure mailgun
 config :preview,
   mailgun_domain: System.get_env("MAILGUN_DOMAIN"),
   mailgun_key:    System.get_env("MAILGUN_KEY")
-
-# Config ecto repo
-config :preview, Preview.Repo,
-  database: "preview_dev",
-  username: "deploy",
-  password: "#{System.get_env("POSTGRES_PASS")}",
-  hostname: "localhost"
-
-# Start Hound for PhantomJS
-config :hound, driver: "phantomjs"
-
-
