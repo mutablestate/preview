c5c46215b1a405550f5fbde59ac46836da296c67
mutablestate <benwebdev@gmail.com>
2015-05-02 04:22:51 +0700

    add asset digests option

--- config/prod.exs
+++ config/prod.exs
@@ -9,6 +9,7 @@ use Mix.Config
 config :preview, Preview.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com"]
+  cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support
 #
