e89e3d8c9df57ae7711b23b6fa5402ddf07c71c0
mutablestate <benwebdev@gmail.com>
2015-05-13 06:03:19 +0700

    update and refactor for csvlixir 2.0.0

--- lib/preview/csv_helper.ex
+++ lib/preview/csv_helper.ex
@@ -1,13 +1,11 @@
 defmodule Preview.CsvHelper do
-
   @extension ".csv"
 
   @doc """
   Checks all headers are atoms and defined within the module struct
-
   Returns boolean or raises ArgumentError
   """
-  @spec valid_headers?(list(atom), map) :: boolean
+  @spec valid_headers?([atom], map) :: boolean
   def valid_headers?(headers, module) do
     if atoms?(headers) == true do
       keys = fetch_keys(module)
@@ -15,7 +13,7 @@ defmodule Preview.CsvHelper do
       headers
       |> Enum.all?(fn header -> Enum.member?(keys, header) end)
     else
-      raise ArgumentError, message: "Header must only contain atoms pre-defined in the passed module struct"
+      raise ArgumentError, message: "Header must contain atoms pre-defined in the passed module struct"
     end
   end
 
@@ -32,49 +30,48 @@ defmodule Preview.CsvHelper do
 
   @doc """
   Generates csv content from a query
-
   Returns data in csv format
   """
-  @spec generate_csv(list(map), module, boolean) :: String.t
+  @spec generate_csv(list(map), [atom], boolean) :: String.t
   def generate_csv(records, headers, return_headers \\ true) do
-    header_row = headers
-      |> Enum.map(fn header -> Atom.to_string(header) end)
-      |> CSVLixir.write
-
-    data_rows = records
-      |> Enum.map(fn record -> Map.take(record, headers) end)
-      |> fetch_values(headers)
-      |> CSVLixir.write
-
-    if return_headers == true do
-      header_row <> "\n" <> data_rows
-    else
-      data_rows
+    case return_headers do
+      true -> header_row(headers) <> data_rows(records, headers)
+      _ -> data_rows(records, headers)
     end
   end
 
-  defp fetch_values(rows, headers) do
+  def header_row(headers) do
+    headers
+    |> Enum.map(fn header -> Atom.to_string(header) end)
+    |> CSVLixir.write_row
+  end
+
+  def data_rows(records, headers) do
+    records
+    |> Enum.map(fn record -> Map.take(record, headers) end)
+    |> fetch_values(headers)
+    |> CSVLixir.write
+    |> Enum.to_list
+    |> Enum.join
+  end
+
+  def fetch_values(rows, headers) do
     rows
     |> Enum.map(fn row ->
-      Enum.map(headers, fn header ->
-        Map.fetch!(row, header)
+        Enum.map(headers, fn header ->
+          Map.fetch!(row, header)
+        end)
       end)
-    end)
   end
 
   @doc """
   Writes csv content to a filename adding a unix timestamp and extension
-
   Returns path to the written file
   """
   @spec csv_to_file(list, String.t) :: String.t
-
   def csv_to_file(csv_content, filename) do
     csv_path = "#{filename}-#{timestamp}" <> @extension
-
-    csv_path
-    |> File.write(csv_content)
-
+    csv_path |> File.write(csv_content)
     csv_path
   end
 
