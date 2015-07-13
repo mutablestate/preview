f45651dc8af1559d0245b66224b5392b551402c4
mutablestate <benwebdev@gmail.com>
2015-07-13 10:48:42 +0700

    use alias for shorter queries

--- web/models/queries.ex
+++ web/models/queries.ex
@@ -1,55 +1,54 @@
 defmodule Preview.Queries do
   import Ecto.Query
+  alias Preview.Repo
   alias Preview.Authenticate
+  alias Preview.Signup
+  alias Preview.User
 
   @doc """
   Query for all users
-
   Returns list of structs
   """
   def all_users do
-    query = from user in Preview.User,
+    query = from user in User,
             select: user
 
-    Preview.Repo.all(query)
+    Repo.all(query)
   end
 
   @doc """
   Query finds user by email
-
   Returns user struct or nil
   """
   def find_by_email(email) do
-    query = from user in Preview.User,
+    query = from user in User,
             where: user.email == ^email,
             select: user
 
-    Preview.Repo.one(query)
+    Repo.one(query)
   end
 
   @doc """
   Query for all signups
-
   Returns list of structs
   """
   def all_signups do
-    query = from signup in Preview.Signup,
+    query = from signup in Signup,
             select: signup
 
-    Preview.Repo.all(query)
+    Repo.all(query)
   end
 
   @doc """
   Query authenticates user
-
   Returns user struct or nil
   """
   def login(email, password) do
-    query = from user in Preview.User,
+    query = from user in User,
             where: user.email == ^email,
             select: user
 
-    user = Preview.Repo.one(query)
+    user = Repo.one(query)
 
     _login(user, password)
   end
